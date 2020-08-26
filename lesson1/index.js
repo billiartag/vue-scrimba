db962699c1251594275e3b4496a36fca1e03ce5d
billiartag <billy.gondokusuma@hotmail.com>
2020-08-26 14:41:11 +0700

    lesson 1

---
+++ lesson1/index.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+   el:"#app", // dimana id object nya
+   data:{
+      message:"Hello Vue" // object message di id app  punya nilai ini
+   }
+})
+app.message= "I have changed"
